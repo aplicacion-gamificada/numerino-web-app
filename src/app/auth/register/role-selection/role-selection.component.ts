e32da8b29357863d87d803da9aac933ad1751230
Sofia Alfaro <alfaros.sofia@gmail.com>
2025-06-01 12:31:21 -0500

    Add: Role selection page

--- src/app/auth/register/role-selection/role-selection.component.ts
+++ src/app/auth/register/role-selection/role-selection.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-role-selection',
@@ -7,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './role-selection.component.scss'
 })
 export class RoleSelectionComponent {
+  constructor(
+    private router: Router,
+    private location: Location
+  ) { }
 
+  navigateToRegister(type: string): void {
+    if (type == 'student') {
+      this.router.navigate(['/student-register']);
+    }
+    else if (type == 'teacher') {
+      this.router.navigate(['/teacher-register']);
+    }
+  }
+
+  goBackToScreenX(): void {
+    console.log('Regresando a la pantalla de Landing...');
+    this.location.back();
+  }
 }
